43e1825463e6599808bb3a7e68539210788c1093
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-04-27 10:53:12 -0400

    Orderby to remove duplicate rows

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -8,3 +8,29 @@ INTO nameyourtable
 FROM _______
 WHERE _______
 ORDER BY _____, _____ DESC;
+
+
+-- Employees_Database_Challenge 
+SELECT employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+titles.title,
+	titles.from_date,
+	titles.to_date
+INTO retirement_titles
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date >= '9999-01-01'
+ORDER BY emp_no ASC;
