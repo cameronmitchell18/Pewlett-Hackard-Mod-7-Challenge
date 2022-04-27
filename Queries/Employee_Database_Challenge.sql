d5d334c761d6eb87e4ecfe0d00ef3f81c0fbd449
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-04-27 12:03:54 -0400

    Deliverable 1 Commit

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -19,18 +19,26 @@ titles.title,
 	titles.to_date
 INTO retirement_titles
 FROM employees
-LEFT JOIN titles
+INNER JOIN titles
 ON employees.emp_no = titles.emp_no
 WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY employees.emp_no ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+retirement_titles.first_name,
+retirement_titles.last_name,
+retirement_titles.title
 
-INTO unique_titles
+--INTO unique_titles
 FROM retirement_titles
-WHERE to_date >= '9999-01-01'
-ORDER BY emp_no ASC;
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC , to_date DESC;
+
+-- employees by jon title who are about to retire 
+SELECT COUNT(unique_titles.emp_no),
+unique_titles.title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT (title) DESC;
